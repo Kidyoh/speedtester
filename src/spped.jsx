f4072335e155c1488f616cadd2b7c54997898f18
Kidyoh <kidyoh789@gmail.com>
2023-11-09 13:55:39 +0300

    major ui changes

--- src/spped.jsx
+++ src/spped.jsx
@@ -1,45 +1,161 @@
 import SpeedTest from '@cloudflare/speedtest';
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import Speedometer, {
+    Background,
+    Arc,
+    Needle,
+    Progress,
+    Marks,
+    Indicator,
+} from 'react-speedometer';
 
 function Speed() {
     const [result, setResult] = useState(null);
     const [running, setRunning] = useState(false);
+    const [ipAddress, setIpAddress] = useState(null);
+    const [currentDownloadSpeed, setCurrentDownloadSpeed] = useState(0);
+    const [currentUploadSpeed, setCurrentUploadSpeed] = useState(0);
 
-    useEffect(() => {
-        const speedTest = new SpeedTest({ autoStart: false });
+    const startSpeedTest = () => {
+        setResult(null);
+        setCurrentDownloadSpeed(null); 
+        setCurrentUploadSpeed(null);
+        const speedTest = new SpeedTest({
+            autoStart: true,
+            measurements: [
+                { type: 'latency', numPackets: 1 },
+                { type: 'download', bytes: 1e5, count: 1, bypassMinDuration: true },
+                { type: 'latency', numPackets: 20 },
+                { type: 'download', bytes: 1e5, count: 9 },
+                { type: 'download', bytes: 1e6, count: 8 },
+                { type: 'upload', bytes: 1e5, count: 8 },
+                { type: 'packetLoss', numPackets: 1e3, responsesWaitTime: 3000 },
+                { type: 'upload', bytes: 1e6, count: 6 },
+            ],
+        });
         speedTest.onRunningChange = isRunning => {
             setRunning(isRunning);
         };
+        fetch('https://api.ipify.org/')
+            .then(response => response.text())
+            .then(data => setIpAddress(data))
+            .catch(error => console.error('Error fetching IP address:', error));
         speedTest.onResultsChange = () => {
             if (!speedTest.isFinished) {
                 const rawResults = speedTest.results.raw;
-                setResult({
-                    ...rawResults,
-                    download: rawResults.download / 1000000,
-                    upload: rawResults.upload / 1000000,
+                const downloadResults = rawResults.download.results;
+                Object.keys(downloadResults).forEach(key => {
+                    const downloadData = downloadResults[key].sideLatency;
+                    downloadData.forEach((item, index) => {
+                        const downloadSpeed = (item.transferSize * 8 * 1024) / (item.payloadDownloadTime / 10) / 1e6;
+                        setCurrentDownloadSpeed(downloadSpeed);
+                    });
                 });
+                const uploadData = rawResults.upload.results;
+                Object.keys(uploadData).forEach(key => {
+                    const uploadItem = uploadData[key].sideLatency;
+                    uploadItem.forEach((item, index) => {
+                        const uploadSpeed = (item.transferSize * 8) / (item.payloadDownloadTime / 10) / 1e6;
+                        setCurrentUploadSpeed(uploadSpeed);
+                    });
+                });
+                console.log(rawResults)
             }
         };
         speedTest.onFinish = results => {
             const summary = results.getSummary();
+            const scores = results.getScores();
             setResult({
                 ...summary,
                 download: summary.download / 1000000,
                 upload: summary.upload / 1000000,
+                scores,
             });
+            console.log(scores)
         };
-        speedTest.play();
-    }, []);
+
+    };
+
 
     return (
-        <div>
-            <h1>Speed Test</h1>
-            <p>{running ? 'Running...' : 'Finished!'}</p>
-            {result && (
-                <pre>{JSON.stringify(result, null, 2)}</pre>
+        <div className="container mx-auto mt-5 p-5">
+            <h1 className="text-2xl mb-4">Speed Test Results</h1>
+            <button
+                onClick={startSpeedTest}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                disabled={running}
+            >
+                Start Test
+            </button>
+            {running ? (
+                <div className="flex items-center m-44 justify-center">
+                    <div className="loading"></div>
+                    <p className="ml-2">Testing...</p>
+                </div>
+            ) : (
+                <div className='flex flex-wrap justify-center'>
+                    <div className='m-3'>
+                        <div style={{ height: '100px' }}>
+                            <Speedometer
+                                value={result ? result.download : currentDownloadSpeed}
+                                fontFamily='squada-one'
+                            >
+                                <Background />
+                                <Arc arcWidth={0.2} />
+                                <Needle />
+                                <Progress />
+                                <Marks />
+                                <Indicator />
+                            </Speedometer>
+                            {result && (
+                                <div className='mt-2 text-center'>
+                                    <p>Download: {result.download.toFixed(2)} Mbps</p>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <div className='m-44'>
+                        <div style={{ height: '100px' }}>
+                            <Speedometer
+                                value={result ? result.upload : currentUploadSpeed}
+                                fontFamily='squada-one'
+                            >
+                                <Background />
+                                <Arc arcWidth={0.2} />
+                                <Needle />
+                                <Progress />
+                                <Marks />
+                                <Indicator />
+                            </Speedometer>
+                            {result && (
+                                <div className='mt-2 text-center'>
+                                    <p>Upload: {result.upload.toFixed(2)} Mbps</p>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    <div>
+                        {ipAddress && (
+                            <p className="mt-8">Your IP Address: {ipAddress}</p>
+                        )}
+                        {result && result.scores && (
+                            <div className='mt-8'>
+                                <h2>AIM Scores:</h2>
+                                <p>Streaming Points: {result.scores.streaming.points}, Classification: {result.scores.streaming.classificationName}</p>
+                                <p>Gaming Points: {result.scores.gaming.points}, Classification: {result.scores.gaming.classificationName}</p>
+                                <p>RTC Points: {result.scores.rtc.points}, Classification: {result.scores.rtc.classificationName}</p>
+                            </div>
+                        )}
+
+
+
+                    </div>
+                </div>
             )}
+
+
         </div>
-    )
+    );
 }
 
-export default Speed
+export default Speed;
